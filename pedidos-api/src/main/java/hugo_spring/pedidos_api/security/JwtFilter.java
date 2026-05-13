639fd80b2f9975d1f693db3ace677fb7443dae02
Hugo-Ferrari <hugoferrari36@gmail.com>
2026-05-13 09:09:21 -0300

    JWT

--- pedidos-api/src/main/java/hugo_spring/pedidos_api/security/JwtFilter.java
+++ pedidos-api/src/main/java/hugo_spring/pedidos_api/security/JwtFilter.java
@@ -22,7 +22,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
         String authHeader = request.getHeader("Authorization");
-        if(authHeader != null && authHeader.startsWith("Bearer")){
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
             String token = authHeader.substring(7);
             if(jwtUtil.validarToken(token)){
                 String email = jwtUtil.extrairEmail(token);
--- pedidos-api/src/main/java/hugo_spring/pedidos_api/security/SecurityConfig.java
+++ pedidos-api/src/main/java/hugo_spring/pedidos_api/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package hugo_spring.pedidos_api.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
